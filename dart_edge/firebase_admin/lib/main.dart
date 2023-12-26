5648963a7d28ae6a7e1077724e55f0ec59ae3b72
akaboshinit <akaboshinit@gmail.com>
2023-12-26 16:24:02 +0900

    fix

--- dart_edge/firebase_admin/lib/main.dart
+++ dart_edge/firebase_admin/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloudflare_workers/cloudflare_workers.dart' hide File;
+import 'package:dart_firebase_admin/auth.dart';
 import 'package:dart_firebase_admin/dart_firebase_admin.dart';
 import 'package:dart_firebase_admin/firestore.dart';
 import 'package:edge_http_client/edge_http_client.dart';
@@ -8,17 +9,17 @@ const clientId = "11****+++++++*";
 const privateKey = "-----BEGIN PRIVATE KEY-----\nMII********";
 const clientEmail = "******@template-dev-d032e.iam.gserviceaccount.com";
 
-
 void main() {
   CloudflareWorkers(fetch: (request, env, ctx) async {
     return http.runWithClient(() async {
       final admin = FirebaseAdminApp.initializeApp(
-          'template-dev-d032e',
-          Credential.fromServiceAccountParams(
-            clientId: clientId,
-            privateKey: privateKey,
-            clientEmail: clientEmail,
-          ));
+        'template-dev-d032e',
+        Credential.fromServiceAccountParams(
+          clientId: clientId,
+          privateKey: privateKey,
+          clientEmail: clientEmail,
+        ),
+      );
       final firestore = Firestore(admin);
       final collection = firestore.collection('test');
       final snapshot = await collection.get();
@@ -26,6 +27,9 @@ void main() {
       for (final doc in snapshot.docs) {
         data.add(doc.data());
       }
+
+      final auth = Auth(admin);
+      await auth.deleteUser("< UserId >");
 
       // レスポンスに必要のないタスクを実行登録しておいて 早くレスポンスする
       ctx.waitUntil(Future(() async {
